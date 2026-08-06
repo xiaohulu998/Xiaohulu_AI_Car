e8261a77a87d41aebaeb3c849c45d3e7baaa7d73
xiaohulu <73844118+xiaohulu998@users.noreply.github.com>
2026-08-06 11:01:14 +0800

    1

---
+++ components/Middlewares/include/onenet_mqtt_key.h
@@ -0,0 +1,31 @@
+#ifndef _MQTT_ONENET_KEY_H_
+#define _MQTT_ONENET_KEY_H_
+
+ /* 产品 ID */
+
+#define ONENET_PRODUCT_ID_KEY    "eGw0Z4EvqV"
+
+/* 产品 access_key */
+
+#define ONENET_PRODUCT_ACCESS_KE_KEY  "jwGVz+tHpVPfhWMBvVOrazcRLTlTeSXF+21fl11DgUs="
+
+/* 设备名称 */
+
+#define ONENET_DEVICE_NAME_KEY "esp32_car"  
+
+/* 平台用户 ID */
+
+#define ONENET_USER_ID_KEY     "538777" 
+
+/* 用户 access_key */
+
+#define ONENET_USER_ACCESS_KE_KEY "b0691c9456f14dbc93d2f84d04767e68"
+
+/* Token 过期时间戳 */
+
+#define TM_EXPIRE_TIME_KEY  1912164540    
+
+
+
+
+#endif
